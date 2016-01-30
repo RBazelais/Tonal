e6f14f975f33591cb7dea55da13b68d76198ffa6
edbles <edbles@gmail.com>
2016-01-30 11:37:30 -0500

    putting unity game base in git
    putting unity game base in git

---
+++ BeadChantGame/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 1000.0F;
+
+	Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+	
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+			Vector2 dir = Vector3.zero;
+			dir.y = Input.acceleration.y;
+			dir.x = Input.acceleration.x;
+			if (dir.sqrMagnitude > 1)
+				dir.Normalize();
+			
+			
+			rb2d.velocity = speed * dir;
+
+}
+}
