c52da7dc98bbfd4798e20f769423afbaac95151e
edbles <edbles@gmail.com>
2016-02-06 18:57:16 -0500

    Fixed Android Alternate compile code
    There was a missing declaration in the alternate android compile script.
    I added that back in.

--- Game2.0/BeadChant/Assets/scripts/Player.cs
+++ Game2.0/BeadChant/Assets/scripts/Player.cs
@@ -40,7 +40,7 @@ public class Player : MonoBehaviour {
 #endif
 
 #if UNITY_ANDROID
-            
+            Vector2 dir = Vector3.zero;
             dir.y = Input.acceleration.y;
             dir.x = Input.acceleration.x;
             //if (dir.sqrMagnitude > 1)
